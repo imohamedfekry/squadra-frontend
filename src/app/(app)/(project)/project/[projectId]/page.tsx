035f396936fdd098b194fef3a4e43fba6f140542
imohamedfekry <mhamedfekry0@gmail.com>
2026-08-18 04:07:18 +0300

    fix: set page titles (Loveble / Home / project name)

--- src/app/(app)/(project)/project/[projectId]/page.tsx
+++ src/app/(app)/(project)/project/[projectId]/page.tsx
@@ -1,6 +1,10 @@
 import { ProjectIdView } from "@/components/project/project-id-view";
 
-export default async function ProjectPage({params}: { params: Promise<{ projectId: string }> }) {
+export default async function ProjectPage({
+  params,
+}: {
+  params: Promise<{ projectId: string }>;
+}) {
   const { projectId } = await params;
 
   return <ProjectIdView projectId={projectId} />;
